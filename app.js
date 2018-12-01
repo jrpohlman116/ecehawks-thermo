0aff25a61c35507ff1b5fc45393e7e870e385063
jlarson7 <joshua-j-larson@uiowa.edu>
2018-12-01 17:26:25 -0600

    First GPIO test

--- app.js
+++ app.js
@@ -9,6 +9,39 @@ var dateTimeRouter = require('./routes/editdatetime');
 
 var app = express();
 
+var http = require('http').createServer(handler); //require http server, and create server with function handler()
+var fs = require('fs'); //require filesystem module
+var io = require('socket.io')(http) //require socket.io module and pass the http object (server)
+var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
+var LED = new Gpio(21, 'out'); //use GPIO pin 4 as output
+
+http.listen(8080); //listen to port 8080
+
+// Error handling
+function handler (req, res) { //create server
+  fs.readFile(__dirname + '/views/index.ejs', function(err, data) { //read file index.html in public folder
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'}); //display 404 on error
+      return res.end("404 Not Found");
+    } 
+    res.writeHead(200, {'Content-Type': 'text/html'}); //write HTML
+    res.write(data); //write data from index.html
+    return res.end();
+  });
+}
+// Sockets control
+io.sockets.on('connection', function (socket) {// WebSocket Connection
+  var lightvalue = 0; //static variable for current status
+
+  socket.on('heat', function(data) { //get light switch status from client
+    lightvalue = data;
+    if (lightvalue != LED.readSync()) { //only change LED if status has changed
+      LED.writeSync(lightvalue); //turn LED on or off
+    }
+  });
+});
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -36,6 +69,12 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+// LED Exports
+process.on('SIGINT', function () { //on ctrl+c
+  LED.writeSync(0); // Turn LED off
+  LED.unexport(); // Unexport LED GPIO to free resources
+  process.exit(); //exit completely
 });
 
 module.exports = app;
