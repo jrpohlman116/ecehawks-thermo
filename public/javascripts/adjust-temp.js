a73d5681b3ea50673f834e7fbe6adfeee71aa543
Jordan Pohlman <jordanpohlman@gmail.com>
2018-12-01 20:17:48 -0600

    adjust the temp on button click

---
+++ public/javascripts/adjust-temp.js
@@ -0,0 +1,15 @@
+function adjustTemp(direction, id){
+    let dialText = document.getElementById(id);
+    let temp = parseInt(dialText.textContent);
+
+    if(direction === 'up'){
+        if(temp !== 80){
+            dialText.innerText = (temp + 1).toString() + '\xB0';
+        }
+    }
+    else if(direction === 'down'){
+        if(temp !== 50){
+            dialText.innerText = (temp - 1).toString() + '\xB0';
+        }
+    }
+}
