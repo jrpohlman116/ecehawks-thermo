068faf6f607afd9d63352c15ffabcd7a81946f33
Jordan Pohlman <jordanpohlman@gmail.com>
2018-12-01 22:26:11 -0600

    change onoff on toggle

--- public/javascripts/set-points.js
+++ public/javascripts/set-points.js
@@ -14,4 +14,20 @@ function onSetPointClick(name) {
     } else {
         x.style.display = "none";
     }
-};
+}
+
+function enableDisableSetPoint() {
+  // Get the checkbox
+  var checkBox = document.getElementById("setpoint-check");
+  // Get the output text
+  var onoff = document.getElementById("onoff-text");
+
+  console.log(onoff);
+
+  // If the checkbox is checked, display the output text
+  if (checkBox.checked == true){
+    onoff.innerText = "ON";
+  } else {
+    onoff.innerText = "OFF";
+  }
+}
