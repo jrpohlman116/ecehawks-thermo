19a9e3c3dd0e0a73ffd6b7e671f320d216c17e18
Jordan Pohlman <jordanpohlman@gmail.com>
2018-12-01 22:42:49 -0600

    remove conditionals
    I am not toggling so there is no need and it avoids a double click

--- public/javascripts/set-points.js
+++ public/javascripts/set-points.js
@@ -1,19 +1,11 @@
 function onDaySelectionClick(name) {
     var x = document.getElementById("schedule-selecter-container");
-    if (x.style.display === "none") {
-        x.style.display = "flex";
-    } else {
-        x.style.display = "none";
-    }
+    x.style.display = "flex";
 } 
 
 function onSetPointClick(name) {
     var x = document.getElementById("enable-edit-container");
-    if (x.style.display === "none") {
-        x.style.display = "flex";
-    } else {
-        x.style.display = "none";
-    }
+    x.style.display = "flex";
 }
 
 function enableDisableSetPoint() {
