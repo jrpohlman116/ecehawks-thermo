07cba42623203c0279516ed2580ad25aaa9eb7ed
Jordan Pohlman <jordanpohlman@gmail.com>
2018-12-02 16:24:50 -0600

    app

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const SocketServer = require('ws').Server;
-var wss = new SocketServer({ server });
+var wss = new SocketServer({ app });
 
 wss.on('connection', (ws) => {
   console.log("Connected")
