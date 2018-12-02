a5508dbecf936195057fae3e319453a80e801ab1
Jordan Pohlman <jordanpohlman@gmail.com>
2018-12-01 23:21:03 -0600

    keep unicode degree symbol consistant from HTML to JS

--- public/javascripts/adjust-temp.js
+++ public/javascripts/adjust-temp.js
@@ -4,12 +4,12 @@ function adjustTemp(direction, id){
 
     if(direction === 'up'){
         if(temp !== 80){
-            dialText.innerText = (temp + 1).toString() + '\xB0';
+            dialText.innerText = (temp + 1).toString() + '\u02DA';
         }
     }
     else if(direction === 'down'){
         if(temp !== 50){
-            dialText.innerText = (temp - 1).toString() + '\xB0';
+            dialText.innerText = (temp - 1).toString() + '\u02DA';
         }
     }
 }
