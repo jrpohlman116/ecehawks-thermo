72e17bb64721759d3baee8c066dcddd3af526221
Jordan Pohlman <jordanpohlman@gmail.com>
2018-12-02 15:21:28 -0600

    remove gettemp and require

--- public/javascripts/main-page.js
+++ public/javascripts/main-page.js
@@ -1,15 +1,3 @@
-const sensor = require('ds18b20-raspi');
-
-function getTemp(){
-  tempF = sensor.readSimpleF(1);
-  tempF = Math.round(tempF)
-
-  console.log(tempF)
-
-  document.getElementById('curr-temp').innerHTML = tempF +  + '\u02DA';
-  var t = setTimeout(getTempandLed, 1000);
-}
-
 function heat() {
     var button = document.getElementById('heat');
     var status = document.getElementById('hvac-status');
