73c92883b390e0ff610ae4bdfb3370fa9e05b171
Jordan Pohlman <jordanpohlman@gmail.com>
2018-12-02 11:06:48 -0600

    remove express and routes folder

--- app.js
+++ app.js
@@ -1,51 +1,86 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var http = require('http').createServer(handler); //require http server, and create server with function handler()
+var fs = require('fs'); //require filesystem module
+var url = require('url');
+var io = require('socket.io')(http) //require socket.io module and pass the http object (server)
+var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
+var HeatLED = new Gpio(21, 'out'); //use GPIO pin 4 as output
+var CoolingLED = new Gpio(20, 'out'); //use GPIO pin 4 as output
+//var pushButton = new Gpio(17, 'in', 'both'); //use GPIO pin 17 as input, and 'both' button presses, and releases should be handled
 
-var indexRouter = require('./routes/index');
-var dateTimeRouter = require('./routes/editdatetime');
-var setPointsRouter = require('./routes/setpoints');
-var setPointTimeRouter = require('./routes/editsetpointtime');
-var setPointTempRouter = require('./routes/editsetpointtemp');
+http.listen(8080); //listen to port 8080
 
-var app = express();
+function handler (req, res) { //create server
+  var path = url.parse(req.url).pathname; // pathname = '/MyApp'
+  switch (path) {
+    case '/':
+      fs.readFile(__dirname + '/views/index.ejs', function(err, data) { //read file index.html in public folder
+        if (err) {
+          res.writeHead(404, {'Content-Type': 'text/html'}); //display 404 on error
+          return res.end("404 Not Found");
+        } 
+        res.writeHead(200, {'Content-Type': 'text/html'}); //write HTML
+        res.write(data); //write data from index.html
+        return res.end();
+      });
+      break;
+    case '/setpoint':
+      fs.readFile(__dirname + '/views/scheduler.ejs', function(err, data) { //read file index.html in public folder
+        if (err) {
+          res.writeHead(404, {'Content-Type': 'text/html'}); //display 404 on error
+          return res.end("404 Not Found");
+        } 
+        res.writeHead(200, {'Content-Type': 'text/html'}); //write HTML
+        res.write(data); //write data from index.html
+        return res.end();
+      });
+      break;
+    case '/setpoint/':
+      fs.readFile(__dirname + '/views/scheduler.ejs', function(err, data) { //read file index.html in public folder
+        if (err) {
+          res.writeHead(404, {'Content-Type': 'text/html'}); //display 404 on error
+          return res.end("404 Not Found");
+        } 
+        res.writeHead(200, {'Content-Type': 'text/html'}); //write HTML
+        res.write(data); //write data from index.html
+        return res.end();
+      });
+    break;
+    default:
+        res.writeHead(404);
+        res.write('Route not defined');
+        res.end();
+}
+  
+}
 
-const port = process.env.PORT || 4000;
-app.listen(port);
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static('public'));
-
-app.use('/', indexRouter);
-app.use('/editdatetime', dateTimeRouter);
-app.use('/setpoints', setPointsRouter);
-app.use('/setpoints/edit/time', setPointTimeRouter);
-app.use('/setpoints/edit/temp', setPointTempRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+io.sockets.on('connection', function (socket) {// WebSocket Connection
+  var lightvalue = 0; //static variable for current status
+/*   pushButton.watch(function (err, value) { //Watch for hardware interrupts on pushButton
+    if (err) { //if an error
+      console.error('There was an error', err); //output error message to console
+      return;
+    }
+    lightvalue = value;
+    socket.emit('light', lightvalue); //send button status to client
+  }); */
+  socket.on('heat', function(data) { //get light switch status from client
+    lightvalue = data;
+    if (lightvalue != HeatLED.readSync()) { //only change LED if status has changed
+      LED.writeSync(lightvalue); //turn LED on or off
+    }
+  });
+  socket.on('ac', function(data) { //get light switch status from client
+    lightvalue = data;
+    if (lightvalue != CoolingLED.readSync()) { //only change LED if status has changed
+      LED.writeSync(lightvalue); //turn LED on or off
+    }
+  });
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+process.on('SIGINT', function () { //on ctrl+c
+  LED.writeSync(0); // Turn LED off
+  LED.unexport(); // Unexport LED GPIO to free resources
+  //pushButton.unexport(); // Unexport Button GPIO to free resources
+  process.exit(); //exit completely
 });
-
-module.exports = app;
--- routes/editdatetime.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render('editdatetime', { title: 'Edit Date/Time', data: '55'});
-});
-
-module.exports = router;
