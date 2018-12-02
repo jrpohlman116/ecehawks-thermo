4dd7ab1b5b56e3535c1086d8e35b77a5ba660f9b
Jordan Pohlman <jordanpohlman@gmail.com>
2018-12-02 16:33:20 -0600

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ var app = express();
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log('Listening on ${ PORT }'))
 
-var wss = new SocketServer({ app });
+var wss = new SocketServer({ port: PORT });
 
 wss.on('connection', (ws) => {
   console.log("Connected")
