5fabd72ff6dfc1605e1b8ac0d862437b4161cd29
Jordan Pohlman <jordanpohlman@gmail.com>
2018-12-01 22:26:55 -0600

    testing out passing data through render

--- routes/editdatetime.js
+++ routes/editdatetime.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('editdatetime', { title: 'Edit Date/Time' });
+  res.render('editdatetime', { title: 'Edit Date/Time', data: '55'});
 });
 
 module.exports = router;
