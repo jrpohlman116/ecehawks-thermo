7593d10c9b170d8839041cf9f8407233d93336ac
austin-lauters <austin-lauters@uiowa.edu>
2018-12-02 20:09:10 -0600

    jp

--- public/client.js
+++ public/client.js
@@ -2,11 +2,10 @@
 console.log('im in here');
 var socket = io.connect('http://localhost:3000');
 
-//query DOM
-var temp = document.getElementById('curr-temp');
+
 
 //listen for events
 socket.on('temp', function(data){
-	temp.innerHTML =  data ;
+	document.getElementById('curr-temp').innerHTML =  data ;
 	console.log(data);
 });
