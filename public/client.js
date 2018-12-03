3d441de5da5099a5c5d7e7a2cde8acb7a6a4afb1
Jordan Pohlman <jordanpohlman@gmail.com>
2018-12-02 21:54:14 -0600

    change eventlisteners to onclick event

--- public/client.js
+++ public/client.js
@@ -18,7 +18,21 @@ var socket = io.connect('http://localhost:3000');
 		auto: 0
 	});
 
-	heatbtn.addEventListener('click', function(){
+	function heat(){
+		var button = document.getElementById('heat');
+		var status = document.getElementById('hvac-status');
+		if (status.innerText === 'Heat On'){
+			status.innerText = 'OFF'
+			button.style.backgroundColor = '#808080'
+			button.value = '0'
+		}else{
+			status.innerText = 'Heat On';
+			button.style.backgroundColor = '#FFCD00'
+			button.value = '1'
+			document.getElementById('auto').style.backgroundColor = '#808080'
+			document.getElementById('ac').style.backgroundColor = '#808080'
+		}
+
 		console.log('heat1 = ' + heatbtn.value);
 		socket.emit('status', {
 			heat: heatbtn.value,
@@ -26,9 +40,23 @@ var socket = io.connect('http://localhost:3000');
 			auto: autobtn.value
 		});
 		console.log('heat2 = ' + heatbtn.value);
-	});	
+	}	
 
-	acbtn.addEventListener('click', function(){
+	function ac(){
+		var button = document.getElementById('ac');
+		var status = document.getElementById('hvac-status');
+		if (status.innerText === 'AC On'){
+			status.innerText = 'OFF'
+			button.style.backgroundColor = '#808080'
+			button.value = '0'
+		}else{
+			status.innerText = 'AC On';
+			button.value = '1'
+			button.style.backgroundColor = '#FFCD00'
+			document.getElementById('auto').style.backgroundColor = '#808080'
+			document.getElementById('heat').style.backgroundColor = '#808080'
+		}
+	
 		console.log('ac1 = ' + heatbtn.value);
 		socket.emit('status', {
 			heat: heatbtn.value,
@@ -38,7 +66,21 @@ var socket = io.connect('http://localhost:3000');
 		console.log('ac2 = ' + acbtn.value);
 	});	
 
-	autobtn.addEventListener('click', function(){
+	function auto(){
+		var button = document.getElementById('auto');
+		var status = document.getElementById('hvac-status');
+		if (status.innerText === 'Auto On'){
+			status.innerText = 'OFF'
+			button.style.backgroundColor = '#808080'
+			button.value = '0'
+		}else{
+			status.innerText = 'Auto On';
+			button.style.backgroundColor = '#FFCD00'
+			button.value = '1'
+			document.getElementById('heat').style.backgroundColor = '#808080'
+			document.getElementById('ac').style.backgroundColor = '#808080'
+		}
+	
 		console.log('auto1 = ' + heatbtn.value);
 		socket.emit('status', {
 			heat: heatbtn.value,
